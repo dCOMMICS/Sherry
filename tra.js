4d72077dd5771ee4764482f222b3f9297b437eb4
dCOMMICS <sanwurld@gmail.com>
2024-04-09 17:32:48 +0300

    array duplication

--- tra.js
+++ tra.js
@@ -1,14 +1,9 @@
-const  person = {
-    name: 'John',
-    age: 34,
-};
+// const { element } = require("three/examples/jsm/nodes/Nodes.js");
 
-for ( const item in person ) {
-    console.log(item)
-}
+const array = [1, 4, 5, "Banana", 6,1,4,4,9,3,"Banana"];
 
+const uniqueElements = array.filter ((element, index) => {
+    return array.indexOf(element) === index
+});
 
-
-
-
-
+console.log(uniqueElements)
