a80d59c5589518364bc7d081bc5426c9160d9964
dCOMMICS <sanwurld@gmail.com>
2024-05-04 00:35:49 +0300

    😈👋🏿

--- ig/GSAPC/blunts.js
+++ ig/GSAPC/blunts.js
@@ -2,4 +2,22 @@
 
  tl.from ("h2",{
     y:-30,
+    opacity:0,
+    duration: 0.9,
+ })
+
+
+ tl.from ("h4",{
+    y:-20,
+    opacity:0,
+    duration: 0.9,
+    stagger: 0.2,
+ })
+
+ tl.from ("h1",{
+    y:-20,
+    opacity:0,
+    duration: 0.9,
+    stagger: 0.3,
+    scale: 0.5,
  })
