1e8ca62e2f80c12fbf7db3b95d3afc838193c88d
dCOMMICS <sanwurld@gmail.com>
2024-05-09 15:28:52 +0300

    👋🏿🙏🏿

--- ig/GSAPC/l2.js
+++ ig/GSAPC/l2.js
@@ -1,5 +1,21 @@
 var tl = gsap.timeline()
 
-tl.from (,{
-    
-}) 
+tl.from("h1", {
+    x: 200,
+    duration: 5,
+    delay: 0
+})
+
+
+
+// gsap.to("h1", {
+//      x:400,
+//      duration:15,
+//      rotate: 185,
+//      color: "rgba(13,175,50)",
+//      backgroundColor: "rgba(12,70,50)",
+//      scale: 2.7,
+//      borderRadius:"50%",
+//      stagger: 1,
+//      delay:1
+// })
