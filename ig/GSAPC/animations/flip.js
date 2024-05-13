8015840ae19c1be21f42d92298ebf8b1a52110c8
dCOMMICS <sanwurld@gmail.com>
2024-05-13 19:50:37 +0300

    initial commit

--- ig/GSAPC/animations/flip.js
+++ ig/GSAPC/animations/flip.js
@@ -1,8 +1,34 @@
-console.clear();
+// console.clear();
+
+// gsap.registerPlugin(Flip, Draggable);
+
+// const fullSize = document.querySelector('.full-Size'), thumbnail = document.querySelector(".thumbnail");
+
+// let bleps = gsap.utils.toArray(".bleps");
+
+
+// bleps.forEach((blep)) =  {
+//     blep.addEventListener("click", () => {
+//         let state = Flip.getState(".bleps");
+//         console.log("GREETING")
+//     }
+// }
+    
+
+console.clear()
 
 gsap.registerPlugin(Flip, Draggable);
 
-const fullSize = document.querySelector(".full-
-        Size"),
-            thumbnail = document.querySelector(".thumbnail")
-    
+const fullSize = document.querySelector(".full-size"),
+      thumbnail = document.querySelector(".thumbnail");
+
+
+let bleps = gsap.utils.toArray('.blep')
+
+bleps.forEach((blep) => {
+  blep.addEventListener("click", () => {
+	console.log('horse')
+});
+
+})
+
