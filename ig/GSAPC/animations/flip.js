b5e95f24f97b0b39ba5d109cd9d8f36825a6e197
dCOMMICS <sanwurld@gmail.com>
2024-05-13 19:20:23 +0300

    initial commit

---
+++ ig/GSAPC/animations/flip.js
@@ -0,0 +1,8 @@
+console.clear();
+
+gsap.registerPlugin(Flip, Draggable);
+
+const fullSize = document.querySelector(".full-
+        Size"),
+            thumbnail = document.querySelector(".thumbnail")
+    
