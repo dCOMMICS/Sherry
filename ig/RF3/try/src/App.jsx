30edf46244097908436efb0d14bd33b06394d54e
dCOMMICS <sanwurld@gmail.com>
2024-05-15 16:32:26 +0300

    initial commit

--- ig/RF3/try/src/App.jsx
+++ ig/RF3/try/src/App.jsx
@@ -7,9 +7,18 @@ const App = () => {
   return (
     <>
      <Canvas>
-      <mesh>
-        <boxGeometry></boxGeometry>
-        <meshStandardMaterial></meshStandardMaterial>
+
+      <directionalLight position={[0,0,2]}/>
+
+      <mesh position={[1,0,0]}>
+        <boxGeometry args={[2,2,4]} />
+        <meshStandardMaterial color={"orange"}/>
+      </mesh>
+
+
+      <mesh position={[1,0,0]}>
+        <boxGeometry args={[1,1,4]} />
+        <meshStandardMaterial color={"orange"}/>
       </mesh>
      </Canvas>
     </>
